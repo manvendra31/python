c2688b6091bd1a3791d3a705d41812b9ad4b0446
manvendra31 <manvendra3131@gmail.com>
2025-08-28 12:50:21 +0530

    update

--- Harvard University/Lecture 6/Import_Q1_pytest.py
+++ Harvard University/Lecture 6/Import_Q1_pytest.py
@@ -1,2 +1,8 @@
-import pytest
+# We will do that by using the Pytest command
 from Question1 import square
+def test_Square():
+    assert square(2)==4
+    assert square(3)==9
+    assert square(-2)==4
+    assert square(-3)==9
+    assert square(0)==0
