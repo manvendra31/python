6385d056299d8a921666148d60904db90a128ff7
manvendra31 <manvendra3131@gmail.com>
2025-09-23 14:25:34 +0530

    .

--- Pratice question/OOPS/SelfParemeter.py
+++ Pratice question/OOPS/SelfParemeter.py
@@ -1,5 +1,16 @@
 # Self parameter is a reference to the current instance of the class. 
 # It allows us to access the attributes and methods of the object.
 
-def doggy():
-    species = "DoeshBhai"
+class Dog:
+    species = "Canine"  # Class attribute
+
+    def __init__(self, name, age):
+        self.name = name  # Instance attribute
+        self.age = age  # Instance attribute
+
+dog1 = Dog("Buddy", 3)  # Create an instance of Dog
+dog2 = Dog("Charlie", 5)  # Create another instance of Dog
+
+print(dog1.name, dog1.age, dog1.species)  # Access instance and class attributes
+print(dog2.name, dog2.age, dog2.species)  # Access instance and class attributes
+print(Dog.species)  # Access class attribute directly
