c54062f5b6b03da8875e86334c64d9e30ab7d3af
manvendra31 <manvendra3131@gmail.com>
2025-12-04 15:13:55 +0530

    a

--- Coursera/Module 2 Data Structure/File/Loop.py
+++ Coursera/Module 2 Data Structure/File/Loop.py
@@ -1,13 +1,11 @@
-# Use the file name mbox-short.txt as the file name
-fh = open(r"D:\python\Coursera\Module 2 Data Structure\File\mob.txt")
 nh = open(r"D:\python\Coursera\Module 2 Data Structure\File\mbox-short.txt")
-for x in fh:
-    print(x)
-
-
-# Counting the line in the file
 
 count = 0
 for line in nh:
-    count = count+1
-print(count)    
+    count += 1
+print(count)
+
+nh = open(r"D:\python\Coursera\Module 2 Data Structure\File\mbox-short.txt")  # reopen
+re = nh.read()
+print(len(re))
+
