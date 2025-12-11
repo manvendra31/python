5ba4b99d43e666070ae1c91236a54e9357ff65c8
manvendra31 <manvendra3131@gmail.com>
2025-12-11 18:23:21 +0530

    .

--- Coursera/Module 2 Data Structure/List/Pratice Question/Question5.py
+++ Coursera/Module 2 Data Structure/List/Pratice Question/Question5.py
@@ -1,1 +1,13 @@
 # Remove the duplicate from the list 
+# Open the file
+with open(r"D:\python\Coursera\Module 2 Data Structure\List\romeo.txt") as f:
+
+    unique_words = []
+
+    for line in f:
+        words = line.split()
+        for w in words:
+            if w not in unique_words:
+                unique_words.append(w)
+
+print(unique_words)
